1a8526e4fde8ea4df35554f9421c7c0155459670
junovso <67838043+junovso@users.noreply.github.com>
2021-12-15 14:16:00 +0100

    Operators
    - Main.py

---
+++ operators/main.py
@@ -0,0 +1,29 @@
+# Do not modify these lines
+__winc_id__ = 'd0d3cdcefbb54bc980f443c04ab3a9eb'
+__human_name__ = 'operators'
+
+# Add your code after this line
+spain_most_spoken = "Castilian Spanish"
+switzerland_most_spoken = "German"
+print (spain_most_spoken == switzerland_most_spoken)
+
+spain_religion = "Catholic"
+switzerland_religion = "Catholic"
+print (spain_religion == switzerland_religion)
+
+spain_capital = "Madrid"
+switzerland_capital = "Bern"
+print (len(spain_capital) != len(switzerland_capital))
+
+spain_gdp = 1281000000000
+switzerland_gdp = 7480000000
+print(switzerland_gdp > spain_gdp)
+
+spain_population_growth = -0.03 
+switzerland_population_growth = 0.65
+print(spain_population_growth and switzerland_population_growth < 1)
+
+spain_population = 47350000
+switzerland_population = 8637000
+print(spain_population > 10000000 or switzerland_population > 10000000)
+print(spain_population > 10000000)
