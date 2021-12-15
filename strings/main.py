29e1637a5fc0776d1b3cb540eabe2048aa7cfa1c
junovso <67838043+junovso@users.noreply.github.com>
2021-12-15 13:17:31 +0100

    changes main.py
    - Changes

--- strings/main.py
+++ strings/main.py
@@ -3,24 +3,30 @@ __winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
 __human_name__ = 'strings'
 
 # Add your code after this line
-player1 = "peter knekkebreud"
-player2 = "danny phantom"
+player1 = "Ruud Gullit"
+player2 = "Marco van Basten"
 goal_0 = 32
 goal_1 = 54
-scorers = f"{player1} scored in minute {goal_0}, \n {player2} finished the match in minute {goal_1}"
+scorers = f"{player1} {goal_0}, {player2} {goal_1}"
+report = f"{player1} scored in the {goal_0}nd minute\n{player2} scored in the {goal_1}th minute"
 print(scorers)
 
 #part 2
 
-player = "peter knekkebreud"
-first_name = player[:5]
+player = "Ruud Gullit"
+first_name = player[:player.find(" ")]
 print(first_name)
 
-lastName = player[player.find(" "):]
-print(len(lastName) - 1)
+last_name = player[player.find(" "):]
+last_name_len = len(last_name) - 1
+print(last_name_len)
 
-shortenedFirstName = player.replace("peter", "P.")
-print(shortenedFirstName)
+name_short = f"{first_name[0]}.{last_name}"
+print(name_short)
 
-chant = (first_name + "!" + " ") * len(first_name)
-print(chant.strip())
+raw_chant = (first_name + "!" + " ") * len(first_name)
+chant = raw_chant.strip()
+print(chant)
+
+good_chant = chant.endswith(" ")
+print(good_chant)
